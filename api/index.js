ff37b2989d159f656c885c3187d4ad9b58668861
unknown <pateldeepanshu196@gmail.com>
2024-04-19 23:35:20 +0530

    URL CHAnges

--- api/index.js
+++ api/index.js
@@ -33,7 +33,7 @@ app.use((req, res, next) => {
   next();
 });
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173" })); //
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true })); //
 app.use(cookieParser());
 
 const storage = multer.diskStorage({
