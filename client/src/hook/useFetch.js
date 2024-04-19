decd81be4c229310b120fc687f7d319b459f415e
unknown <pateldeepanshu196@gmail.com>
2024-04-19 23:48:44 +0530

    URL CHAnges

--- client/src/hook/useFetch.js
+++ client/src/hook/useFetch.js
@@ -5,7 +5,7 @@ import { AuthContext } from "../context/authContext";
 const useMakeRequest = () => {
   const { currentUser } = useContext(AuthContext);
   const makeRequest = axios.create({
-    baseURL: import.meta.env.SERVER_URL,
+    baseURL: import.meta.env.VITE_SERVER_URL,
     withCredentials: true,
     headers: {
       Authorization: `Bearer ${currentUser.token}`,
